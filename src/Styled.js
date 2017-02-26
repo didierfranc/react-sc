d96a0acc34ca0cd9f13d6b9a0ba3fc7d790c8da6
Didier Franc <didierfranc@me.com>
2017-02-26 03:04:15 +0100

    Add innerRef to access ref of styled components Also split process.js and simplify again and again

---
+++ src/Styled.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default (T, h) => {
+  const X = <T />
+
+  const C = (props) => {
+
+    const newProps = {
+      ...props,
+      className: props.className ? props.className + ' ' + h : h,
+    }
+
+    if (newProps.innerRef) newProps.ref = props.innerRef
+    if (typeof X.type === 'string') delete newProps.innerRef
+
+    return <X.type {...newProps} />
+  }
+
+  C.displayName = `${T.name || T}-${h}`
+
+  return C
+}
